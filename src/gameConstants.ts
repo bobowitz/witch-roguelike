89f5d08f8c6b81eeb17110fbba7ce8826658dd5a
Alton Olson <bobowitzbob@gmail.com>
2017-08-08 00:36:07 -0700

    added map

--- src/gameConstants.ts
+++ src/gameConstants.ts
@@ -1,7 +1,7 @@
 import { LevelConstants } from "./levelConstants";
 
 export class GameConstants {
-  static readonly VERSION = "v0.0.3";
+  static readonly VERSION = "v0.0.4";
 
   static readonly FPS = 60;
 
