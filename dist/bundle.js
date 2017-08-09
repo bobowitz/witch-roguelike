c2bdff1076f849738cbba8f87b84fde4c79c83bd
Alton Olson <bobowitzbob@gmail.com>
2017-08-09 15:23:30 -0700

    v0.0.16

--- dist/bundle.js
+++ dist/bundle.js
@@ -162,7 +162,7 @@ var levelConstants_1 = __webpack_require__(4);
 var GameConstants = (function () {
     function GameConstants() {
     }
-    GameConstants.VERSION = "v0.0.15";
+    GameConstants.VERSION = "v0.0.16";
     GameConstants.FPS = 60;
     GameConstants.TILESIZE = 16;
     GameConstants.SCALE = 2;
