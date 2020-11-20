581e992282953fbf36fda16ddd458601f45284b7
Alton Olson <altonolson35@gmail.com>
2020-11-20 14:05:57 -0500

    version number+server address

--- src/gameConstants.ts
+++ src/gameConstants.ts
@@ -1,7 +1,7 @@
 import { LevelConstants } from "./levelConstants";
 
 export class GameConstants {
-  static readonly VERSION = "v0.5.8";
+  static readonly VERSION = "v0.6.0";
 
   static readonly FPS = 60;
   static readonly ALPHA_ENABLED = true;
